40fe8502d15c2e0b0c740490b685d2eeb5439c14
AjaBurnett <burnettaja@gmail.com>
2018-10-17 09:08:20 -0400

    second commit

--- src/Lab8.java
+++ src/Lab8.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Lab8 {
 
+	//Lab 8 Class Roster
+	//Aja Burnett
+	//17-October-18
 	
 	public static void main(String[] args) {
 	
@@ -14,13 +17,15 @@ public class Lab8 {
 		String[] studentName = {"Charlotte", "Aja", "Christie", "Imran",  "Carl", "Anthony"};
 		String [] favoriteFood = { "Savory Fricassed Crow...or any crow that's not rusted", "anything free", "French Fries. She can smell fried potatoes a mile away.", "chicken, or at least that's what I gathered while evesdropping.", "not chicken. Maybe applesauce", "Pizza stuffed with gravy, or whatever that thing he was eating was" };
 		String [] hometown = {"Seameet", "Torrine", "Bardford", "Gramsby", "Bailymena", "Knife's Edge"};
-		String prompt;
+		String prompt; //to terminate or repeat the program
 		
 		
 		
 		
 		System.out.println("Welcome to our Java Class. This is our class roster!)");
-	
+	 
+		
+		//initial class roster to identify students
 		for (int i = 0; i<studentName.length; i++) {
 			System.out.println((i+1) + "." + studentName[i]);
 		}
@@ -29,9 +34,9 @@ public class Lab8 {
 		do {
 		
 		
-		int userOption = SampleValidator.getInt(scan, "\nPick a student: ", 1, studentName.length);
+		int userOption = SampleValidator.getInt(scan, "\nPick a student: ", 1, studentName.length); //viewable in class "Sample Validator" in package
 	
-		System.out.println(studentName[userOption-1] + "'s favorite food is " + favoriteFood[userOption-1] + ". Do you want to know their hometown (Courtesy of FantasyNameGenerators.com)?");
+		System.out.println(studentName[userOption-1] + "'s favorite food is " + favoriteFood[userOption-1] + ". Do you want to know their hometown (Courtesy of FantasyNameGenerators.com)?(yes/no)");
 		 
 		String answer;
 		answer = scan.next();
@@ -42,9 +47,9 @@ public class Lab8 {
 		System.out.println("Would you like to get to know another Student? (yes/no)");
 		Scanner s = new Scanner(System.in);
 		prompt = s.next();
-	    }while(prompt.equalsIgnoreCase("yes")); 
+	    }while(prompt.equalsIgnoreCase("yes")); //loops program
 	     
-		if (prompt.equalsIgnoreCase("no"));
+		if (prompt.equalsIgnoreCase("no")); //terminates program
 		System.out.println("Thanks for visiting!");
 		
 		
